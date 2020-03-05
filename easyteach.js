1b46dfc0649c69e805688bb17882812e6ac3e20f
po4599 <52984452+po4599@users.noreply.github.com>
2020-03-04 17:52:56 -0800

    added reference to external js files.

--- easyteach.js
+++ easyteach.js
@@ -1,3 +1,5 @@
+<script src="https://po4599.github.io/javascript/grid.js"></script>
+<script src="https://po4599.github.io/javascript/colorpicker.js"></script>
 function easyTeachSetup() {
     colorPickerSetup();
 }
